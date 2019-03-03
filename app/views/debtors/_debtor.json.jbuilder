e02403cc1f7c19ae9e3a447adc5caad7198b7fc7
jhuang60 <jhuang60@hawk.iit.edu>
2019-03-03 15:01:06 -0600

    Generate scaffold controller
    $ rails g scaffold_controller debtor

---
+++ app/views/debtors/_debtor.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! debtor, :id, :created_at, :updated_at
+json.url debtor_url(debtor, format: :json)
---
+++ test/system/debtors_test.rb
@@ -0,0 +1,41 @@
+require "application_system_test_case"
+
+class DebtorsTest < ApplicationSystemTestCase
+  setup do
+    @debtor = debtors(:one)
+  end
+
+  test "visiting the index" do
+    visit debtors_url
+    assert_selector "h1", text: "Debtors"
+  end
+
+  test "creating a Debtor" do
+    visit debtors_url
+    click_on "New Debtor"
+
+    click_on "Create Debtor"
+
+    assert_text "Debtor was successfully created"
+    click_on "Back"
+  end
+
+  test "updating a Debtor" do
+    visit debtors_url
+    click_on "Edit", match: :first
+
+    click_on "Update Debtor"
+
+    assert_text "Debtor was successfully updated"
+    click_on "Back"
+  end
+
+  test "destroying a Debtor" do
+    visit debtors_url
+    page.accept_confirm do
+      click_on "Destroy", match: :first
+    end
+
+    assert_text "Debtor was successfully destroyed"
+  end
+end
