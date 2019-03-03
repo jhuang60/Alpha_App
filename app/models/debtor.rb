d86dcc7cfca429fd5c15618a2ba02961a3959dd5
jhuang60 <jhuang60@hawk.iit.edu>
2019-03-02 18:09:33 -0600

    add validation to debtor.rb

--- app/models/debtor.rb
+++ app/models/debtor.rb
@@ -1,3 +1,12 @@
 class Debtor < ApplicationRecord
   has_many :debt
+  validates :name, presence: true
+  validates :email, format: {
+    with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i,
+    message: 'Invalid email'
+  }
+  validates :phone_num, format: {
+    with: /\d{3}-\d{3}-\d{4}/,
+    message: "Invalid phone number" 
+  }
 end
