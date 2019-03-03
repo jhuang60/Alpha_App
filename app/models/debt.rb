197a29ed2ad31796acea3e5726d89d76b20eacf3
jhuang60 <jhuang60@hawk.iit.edu>
2019-03-02 18:16:11 -0600

    validations added for debt.rb and modified for debtor.rb

--- app/models/debt.rb
+++ app/models/debt.rb
@@ -1,3 +1,4 @@
 class Debt < ApplicationRecord
   belongs_to :debtor
+  validates :amount, :date_borrowed, :date_due, :description presence: true
 end
--- app/models/debtor.rb
+++ app/models/debtor.rb
@@ -1,12 +1,15 @@
 class Debtor < ApplicationRecord
   has_many :debt
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
+  validates :email, :phone_num, allow_blank: true, uniqueness: true
+
   validates :email, format: {
     with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i,
-    message: 'Invalid email'
+    message: 'Invalid. Please double check the email entered.'
   }
   validates :phone_num, format: {
     with: /\d{3}-\d{3}-\d{4}/,
-    message: "Invalid phone number" 
+    message: "Invalid. Please double check the number entered."
   }
+  
 end
