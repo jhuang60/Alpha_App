e76ccb6894e4dafefbac96da01f933612de36034
jhuang60 <jhuang60@hawk.iit.edu>
2019-03-03 18:19:28 -0600

    Generate scaffold for debt
    $ rails generate scaffold_controller debt

---
+++ app/views/debts/_debt.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! debt, :id, :created_at, :updated_at
+json.url debt_url(debt, format: :json)
---
+++ test/system/debts_test.rb
@@ -0,0 +1,41 @@
+require "application_system_test_case"
+
+class DebtsTest < ApplicationSystemTestCase
+  setup do
+    @debt = debts(:one)
+  end
+
+  test "visiting the index" do
+    visit debts_url
+    assert_selector "h1", text: "Debts"
+  end
+
+  test "creating a Debt" do
+    visit debts_url
+    click_on "New Debt"
+
+    click_on "Create Debt"
+
+    assert_text "Debt was successfully created"
+    click_on "Back"
+  end
+
+  test "updating a Debt" do
+    visit debts_url
+    click_on "Edit", match: :first
+
+    click_on "Update Debt"
+
+    assert_text "Debt was successfully updated"
+    click_on "Back"
+  end
+
+  test "destroying a Debt" do
+    visit debts_url
+    page.accept_confirm do
+      click_on "Destroy", match: :first
+    end
+
+    assert_text "Debt was successfully destroyed"
+  end
+end
