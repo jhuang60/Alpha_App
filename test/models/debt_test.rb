280a6c0db20b9610f78c4746ce796afeeb3266b5
jhuang60 <jhuang60@hawk.iit.edu>
2019-03-03 18:30:30 -0600

    adding test models

--- test/models/debt_test.rb
+++ test/models/debt_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class DebtTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "debt attributes must not be empty" do
+    debt = Debt.new
+    assert debt.invalid?
+    assert debt.errors[:amount].any?
+    assert debt.errors[:date_borrowed].any?
+    assert debt.errors[:date_due].any?
+    assert debt.errors[:description].any?
+  end
 end
--- test/models/debtor_test.rb
+++ test/models/debtor_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class DebtorTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "debtor attributes must not be empty" do
+    debtor = Debtor.new
+    assert debtor.invalid?
+    assert debtor.errors[:name].any?
+    assert debtor.errors[:email].any?
+    assert debtor.errors[:phone_num].any?
+  end
 end
