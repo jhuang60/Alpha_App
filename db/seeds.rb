5227c7f35a23b054ee9e9965508e553bb6579134
jhuang60 <jhuang60@hawk.iit.edu>
2019-03-03 18:16:15 -0600

    changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,20 +13,20 @@ debtor.destroy_all
   name: 'Jessica Ramirez',
   email: 'jessica@gmail.com',
   phone_num: '312-283-2846',
-  debtor_id: 1
+  debtor_id: '1'
 )
 
 @James = debtor.create (
   name: 'James Smith',
   email: 'Jamess@gmail.com',
   phone_num: '312-324-2344',
-  debtor_id: 2
+  debtor_id: '2'
 )
 
 
 debt.create(
   [
-    {amount: '5.00', dete_borrowed: '12-29-2018', date_due: '01-20-2019', description: 'restaurant bill money', debtor_id: 1, debtor: @Jessica}
-    {amount: '50.00', dete_borrowed: '01-01-2019', date_due: '01-30-2019', description: 'electric bill money', debtor_id: 2, debtor: @James}
+    {amount: '5.00', dete_borrowed: '12-29-2018', date_due: '01-20-2019', description: 'restaurant bill money', debtor_id: '1', debtor: @Jessica}
+    {amount: '50.00', dete_borrowed: '01-01-2019', date_due: '01-30-2019', description: 'electric bill money', debtor_id: '2', debtor: @James}
   ]
 )
