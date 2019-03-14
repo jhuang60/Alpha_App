16e9d9fb9d60b6d5e1335e3e17a844bacc7c7655
jhuang60 <jhuang60@hawk.iit.edu>
2019-03-14 12:24:07 -0500

    adding getters and setters

--- app/models/debt.rb
+++ app/models/debt.rb
@@ -1,4 +1,29 @@
 class Debt < ApplicationRecord
   belongs_to :debtor
   validates :amount, :date_borrowed, :date_due, :description, presence: true
+
+  def debtor_name=(name)
+    self.debtor = Debtor.find_or_create_by(name: name)
+  end
+
+  def debtor_name
+    self.debtor ? self.debtor.name : nil
+  end
+
+  def debtor_email=(email)
+    self.debtor = Debtor.find_or_create_by(email: email)
+  end
+
+  def debtor_email
+    self.debtor ? self.debtor.email : nil
+  end
+
+  def debtor_phone_num=(phone_num)
+    self.debtor = Debtor.find_or_create_by(phone_num: phone_num)
+  end
+
+  def debtor_phone_num
+    self.debtor ? self.debtor.phone_num : nil
+  end
+
 end
--- test/models/debt_test.rb
+++ test/models/debt_test.rb
@@ -9,4 +9,41 @@ class DebtTest < ActiveSupport::TestCase
     assert debt.errors[:date_due].any?
     assert debt.errors[:description].any?
   end
+
+  test "debts responds to debtor_name getter" do
+   @debt = debts(:Jessica)
+   assert_equal @debt.debtor_name, debts(:Jessica).debtor.name
+ end
+
+ test "debts responses to debtor_name setter" do
+   @debt = debts(:Jessica)
+   @debt.debtor_name = debtors(:Jessica_Ramirez).name
+   @debt.save
+   assert_equal @debt.debtor_name, debtors(:Jessica_Ramirez).name
+ end
+
+ test "debts responds to debtor_email getter" do
+  @debt = debts(:Jessica)
+  assert_equal @debt.debtor_email, debts(:Jessica).debtor.email
+end
+
+ test "debts responses to debtor_email setter" do
+  @debt = debts(:Jessica)
+  @debt.debtor_email = debtors(:Jessica_Ramirez).email
+  @debt.save
+  assert_equal @debt.debtor_email, debtors(:Jessica_Ramirez).email
+end
+
+ test "debts responds to debtor_phone_num getter" do
+   @debt = debts(:Jessica)
+   assert_equal @debt.debtor_phone_num, debts(:Jessica).debtor.phone_num
+end
+
+ test "debts responses to debtor_phone_num setter" do
+   @debt = debts(:Jessica)
+   @debt.debtor_phone_num = debtors(:Jessica_Ramirez).phone_num
+   @debt.save
+ assert_equal @debt.debtor_phone_num, debtors(:Jessica_Ramirez).phone_num
+end
+
 end
